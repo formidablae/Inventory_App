447d37202786127c224bee84fd9185ee96e441d4
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-04 00:58:00 +0100

    Added Clarity library styles, scripts and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 
@@ -10,7 +12,9 @@ import { ProductsComponent } from './products/products.component';
     ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
