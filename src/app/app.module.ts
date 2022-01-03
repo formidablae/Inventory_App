5ce5b26b7b105553941c8e4392e957c2e69da63e
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-03 22:38:10 +0100

    Added Products component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
